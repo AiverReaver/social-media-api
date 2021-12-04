4837c2db694a284fe214eb2164f5b535b66a797b
AiverReaver <ashishvagish@outlook.in>
2021-12-04 17:45:08 +0530

    now getting db connection form env

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
